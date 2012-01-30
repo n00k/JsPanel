6f08654f7596ca948d7f084223bcd7bb55807abb
n00k <n00k at github.com>
2012-01-29 20:53:30 -0700

    couple of buttons, fun with themes via the Gimp

--- JsPanel.js
+++ JsPanel.js
@@ -2,14 +2,16 @@ function JsPanel()
 {
 	this.pressX = 0;
 	this.pressY = 0;
-	this.state = {pressed:false,resizing:false,moving:false,dx:0,dy:0};
+	this.state = {pressed:false,resizing:false,moving:false,minimized:false,dx:0,dy:0,x:'20px',y:'20px',ht:'401px',wd:'251px'};
 	this.listening = false;
 	this.element = null;
 	this.maindiv = null;
-	this.imgdir = 'greenback';
+	this.panelhead = null;
+	this.title = {element:null,text:" "};
+	this.themedir = 'greenback';
 }
 
-JsPanel.prototype.resize = function(w,h) {
+JsPanel.prototype.resize = function(h,w) {
 	var ht = h || this.element.clientHeight;
 	var wd = w || this.element.clientWidth;
 	var height = parseInt(ht);
@@ -17,6 +19,8 @@ JsPanel.prototype.resize = function(w,h) {
 	ret = false;
 	if ( height > 63 && width > 71 ) {
 		ret = true;
+		this.state.ht = height;
+		this.state.wd = width;
 		this.element.style.height = height + 'px';
 		this.element.style.width = width + 'px';
 		var divs = this.element.getElementsByTagName('div');
@@ -38,44 +42,81 @@ JsPanel.prototype.resize = function(w,h) {
 	return ret;
 }
 
+JsPanel.prototype._addEventListener = function(element, eventtype, func, capture)
+{
+	if (element.addEventListener) {
+		return element.addEventListener(eventtype, func, capture);
+	} else {
+		var ret = element.attachEvent('on' + eventtype,func);
+		if (capture) element.setCapture();
+		return ret;
+	}
+}
+
+JsPanel.prototype.minimize = function()
+{
+	var width = parseInt(this.element.clientWidth);
+	var height = this.panelhead.clientHeight + this.panelhead.offsetTop;
+	this.state.minimized = true;
+	this.state.width = this.element.clientWidth;
+    	this.state.height = this.element.clientHeight;
+	this.element.style.height = height + 'px';
+	this.element.style.width = width + 'px';
+	this.element.className = 'JsPanel_minimized';
+}
+
+JsPanel.prototype.maximize = function()
+{
+	this.state.minimized = false;
+	this.resize(this.state.ht,this.state.wd);
+	this.element.className = 'JsPanel_container';
+}
+
 JsPanel.prototype.moveBy = function (dx,dy)
 {
-	console.log("moveBy(" + dx + "," + dy + ")");
 	var curx = (this.element.style.left.length == 0 )?this.element.offsetLeft:this.element.style.left;
 	var cury = (this.element.style.top.length == 0 )?this.element.offsetTop:this.element.style.top;
 	var x = parseInt(curx) + parseInt(dx);
 	var y = parseInt(cury) + parseInt(dy);
 	var ret = {x:0,y:0,moved:false};
-	if (x > 0 && ((x + this.element.clientWidth) < window.innerWidth)) {
-		this.element.style.left = x + 'px';
+//	if (x > 0 && ((x + this.element.clientWidth) < window.innerWidth)) {
+		this.state.x = x + 'px';
+		this.element.style.left = this.state.x;
 		ret.x = 1;
 		ret.moved = true;
-	}
-	if (y > 0 && ((y + this.element.clientHeight) < window.innerHeight)) {
-		this.element.style.top = y + 'px';
+//	}
+//	if (y > 0 && ((y + this.element.clientHeight) < window.innerHeight)) {
+		this.state.y = y + 'px';
+		this.element.style.top = this.state.y;
 		ret.y = 1;
 		ret.moved = true;
-	}
+//	}
 	return ret;
 }
 
 JsPanel.prototype.mousePressed = function(evnt, source)
 {
+	if (!evnt) { evnt = window.event; }
 	var el = evnt.currentTarget || evnt.srcElement;
-	if (el == this.element && source == "panel") {
-		this.pressX = evnt.screenX;
-		this.pressY = evnt.screenY;
+	if (source == "panel") {
+		this.pressX = evnt.clientX;
+		this.pressY = evnt.clientY;
 		this.state.pressed = !this.state.pressed;
 	} else if (source == "body") {
 		this.state.pressed = false;
+		this.element.className = this.element.className.replace(/[ ]*JsPanel_pressed[ ]*/,"");
 	}
-	console.log(el.tagName + ":" + el.className + "." + evnt.type + "-" + this.state.pressed + " (" + this.state.dx + "," + this.state.dy + ")" + this.state.resizing);
+	this.element.className = this.element.className.replace(/[ ]*JsPanel_pressed[ ]*/,"");
+	if (this.state.pressed) {
+		this.element.className += " JsPanel_pressed";
+	}
 	evnt.cancelBubble = true;
 	if (evnt.stopPropagation) evnt.stopPropagation(); 
 }
 
 JsPanel.prototype.mouseReleased = function(evnt)
 {
+	if (!evnt) { evnt = window.event; }
 	var el = evnt.currentTarget || evnt.srcElement;
 
 	if (!this.state.resizing) {
@@ -89,32 +130,34 @@ JsPanel.prototype.mouseReleased = function(evnt)
 	} 
 }
 
-JsPanel.prototype.mouseMoved = function(evnt)
+JsPanel.prototype.mouseMoved = function(evnt,source)
 {
 	if (!evnt) { evnt = window.event; }
 	var el = evnt.currentTarget || evnt.srcElement;
-	var x = evnt.layerX || evnt.offsetX;
-	var sx = evnt.screenX;
-	var y = evnt.layerY || evnt.offsetY;
-	var sy = evnt.screenY;
+	var sx = evnt.clientX;
+     var sy = evnt.clientY;
+	var x = sx - parseInt(this.state.x);
+	var y = sy - parseInt(this.state.y);
 	var dx = (sx - this.pressX);
 	var dy = (sy - this.pressY);
-//	console.log(el.tagName + ":" + el.className + " " + this.state.pressed);
-//     console.log("(" + x + "," + y + ") (" + evnt.clientX + "," + evnt.clientY + ") : " + dx + "," + dy);
-//	console.log(evnt);
+	var msg = "";
+	for (key in this.state) {
+		msg += key + ": " + this.state[key] + ", ";
+	}
+	msg += "<br>" + evnt.type + "(" + source + ") - target:" + el.tagName + "#" + el.id + "." + el.className;
+	document.getElementById('debugout').innerHTML = msg;
 	if (this.state.pressed) {
-			console.log(this.state.resizing + " (" + dx + "," + dy + ")");
 		if (this.state.resizing) {
 			var moved = {x:1,y:1,moved:true};
 			if ((this.state.resizing.match(/^n/) && dy != 0) || (this.state.resizing.match(/^w/) && dx != 0)) {
 				moved = this.moveBy((this.state.dx == 0)?0:dx,(this.state.dy == 0)?0:dy);
 			}
-			if ( moved.moved && !this.resize(this.element.clientWidth + (dx * this.state.dx * moved.x), this.element.clientHeight + (dy * this.state.dy * moved.y))) this.moveBy((this.state.dx == 0)?0:0-dx,(this.state.dy == 0)?0:0-dy);;
+			if ( moved.moved && !this.resize(this.element.clientHeight + (dy * this.state.dy * moved.y),this.element.clientWidth + (dx * this.state.dx * moved.x))) this.moveBy((this.state.dx == 0)?0:0-dx,(this.state.dy == 0)?0:0-dy);;
 		} else if (this.state.moving) {
 			this.moveBy(dx,dy);
 		}
 	} else {
-		if (el == this.element) {
+		if (source == "panel" && !this.state.minimized) {
 			if (x < 10 && y < 10) {
 				this.state.dx = -1;
 				this.state.dy = -1;
@@ -159,6 +202,8 @@ JsPanel.prototype.mouseMoved = function(evnt)
 				this.state.resizing = false;
 				el.style.cursor = 'auto';
 			}
+		} else {
+			this.element.style.cursor = 'auto';
 		}
 	}
 	this.pressX = sx;
@@ -174,7 +219,7 @@ JsPanel.prototype._addElement = function (parentel, tag, id, cssclass, attrs)
 	if (cssclass != null) newel.className = cssclass;
 	if (typeof attrs == "object") {    
 		for (var key in attrs) {
-			newel[key] = attrs[key];
+			newel.setAttribute(key,attrs[key]);
 		}
 	}
 	if (parentel && parentel.constructor.toString().match(/HTML([a-zA-Z0-9]*)Element/)) parentel.appendChild(newel);
@@ -187,6 +232,58 @@ JsPanel.prototype.addElement = function (tag, id, cssclass, attrs)
 	return this._addElement(this.maindiv, tag, id, cssclass, attrs);
 }
 
+JsPanel.prototype.cancelEvent = function (evnt)
+{
+	if (!evnt) { evnt = window.event; }
+  	evnt.cancelBubble = true;
+	if (evnt.stopPropagation) evnt.stopPropagation();   
+}
+
+
+JsPanel.prototype.buttonSwitch = function (evnt, up, cancel)
+{
+	if (!evnt) { evnt = window.event; }
+	var el = evnt.currentTarget || evnt.srcElement;
+	el.className = el.className.replace(/_down$/,"");
+	if (!up) el.className = el.className + '_down';
+	if (cancel) this.cancelEvent(evnt);
+}
+
+JsPanel.prototype.panelHeadButton = function (element,type,actionfunc)
+{
+	var newel = this._addElement(element,'button','JsPanel_' + type + 'btn','JsPanel_' + type + 'btn',{type:'button'});
+	var thisinst = this;
+	this._addEventListener(newel,'mousedown',function (event) {thisinst.buttonSwitch(event,false,true)});
+	this._addEventListener(newel,'mouseup',function (event) {thisinst.buttonSwitch(event,true,true)});
+	this._addEventListener(newel,'mouseout',function (event) {thisinst.buttonSwitch(event,true,true)});
+	this._addEventListener(newel,'click',function (event) {thisinst[actionfunc]();thisinst.cancelEvent(event)});
+	this._addEventListener(newel,'mousemove',function (event) {thisinst.mouseMoved(event,"button")}, true);
+}
+
+JsPanel.prototype.wipe = function()
+{
+	this.maindiv.innerHTML = "";           
+	this.panelhead = this.addElement('div','JsPanel_panelhead','JsPanel_panelhead');
+	this.panelHeadButton(this.panelhead,'close','hide');
+	this.panelHeadButton(this.panelhead,'min','minimize');
+	this.panelHeadButton(this.panelhead,'max','maximize');
+
+	var tspan = this._addElement(this.panelhead,'span','JsPanel_title_span','JsPanel_title_span');
+	this.title.element = this._addElement(tspan,'textNode','JsPanel_title_text',null,{textContent:this.title.text});
+}
+
+JsPanel.prototype.setTheme = function(themedir)
+{
+	var link = document.getElementById('JsPanel_Theme_Link');
+ 	if (link) {
+		document.getElementsByTagName('head')[0].removeChild(link);
+	}
+	this.themedir = themedir;
+	this._addElement(document.getElementsByTagName('head')[0], 'link', 'JsPanel_Theme_Link',null,{type:'text/css',href:this.themedir + '/JsPanel_theme.css',rel:'stylesheet'}); 
+	var bkgd = document.getElementById("JsPanel_midback");
+	if (bkgd) bkgd.src = this.themedir + '/mid.png';
+}
+
 JsPanel.prototype.buildPanel = function(x,y,ht,wd)
 {
 	if (this.element && this.element.constructor.toString().match(/HTML([a-zA-Z0-9]*)Element/)) {
@@ -196,44 +293,64 @@ JsPanel.prototype.buildPanel = function(x,y,ht,wd)
 		this.element = null;
 	}
 	var thisinst = this;
-	var el = this._addElement(null, 'div', 'JsPanel', 'JsPanel_hidden', {
-	    onmousemove:function (event) {thisinst.mouseMoved(event)},
-	    onmouseup:function (event) {thisinst.mouseReleased(event)},
-	    onmousedown:function (event) {thisinst.mousePressed(event,"panel")},
-	    onmouseout:function (event) {thisinst.mouseMoved(event)},
-	    onmouseover:function (event) {thisinst.mouseMoved(event)},
-	    onclick:function (event) {return}
-	});
+	var el = this._addElement(null, 'div', 'JsPanel', 'JsPanel_hidden');
+	this._addEventListener(el,'mousemove',function (event) {thisinst.mouseMoved(event,"panel")});
+	this._addEventListener(el,'mouseup',function (event) {thisinst.mouseReleased(event)});
+	this._addEventListener(el,'mousedown',function (event) {thisinst.mousePressed(event,"panel")});
+	this._addEventListener(el,'mouseout',function (event) {thisinst.mouseMoved(event,"panel")});
+	this._addEventListener(el,'mouseover',function (event) {thisinst.mouseMoved(event,"panel")});
+	this._addEventListener(el,'click',function (event) {return});
 	var left = this._addElement(el,'div','JsPanel_left', 'JsPanel_left');
 	this._addElement(left,'div','JsPanel_ul', 'JsPanel_ul');
 	this._addElement(left,'div','JsPanel_leftmid', 'JsPanel_leftmid');
 	this._addElement(left,'div','JsPanel_ll', 'JsPanel_ll');
 	var mid = this._addElement(el,'div','JsPanel_mid', 'JsPanel_mid');
 	this._addElement(mid,'div','JsPanel_midupper', 'JsPanel_midupper');
-	this.maindiv = this._addElement(mid,'div','JsPanel_midmain', 'JsPanel_midmain');
-	this._addElement(this.maindiv,'img','JsPanel_midback','JsPanel_midback',{src:this.imgdir + '/mid.png'});
+	var midmain = this._addElement(mid,'div','JsPanel_midmain', 'JsPanel_midmain');
+	this._addElement(midmain,'img','JsPanel_midback','JsPanel_midback',{src:this.themedir + '/mid.png'});
 	this._addElement(mid,'div','JsPanel_midlower', 'JsPanel_midlower');
 	var right = this._addElement(el,'div','JsPanel_right', 'JsPanel_right');
 	this._addElement(right,'div','JsPanel_ur', 'JsPanel_ur');
 	this._addElement(right,'div','JsPanel_rightmid', 'JsPanel_rightmid');
 	this._addElement(right,'div','JsPanel_lr', 'JsPanel_lr');
+	this.maindiv = this._addElement(el,'div','JsPanel_content','JsPanel_content');
 	this.element = el;
-	this._addElement(document.head, 'link', 'JsPanel_Link',null,{type:'text/css',href:'JsPanel.css',rel:'stylesheet'});
-	this.element.className = 'JsPanel_container';
-	document.body.addEventListener('mousemove',function (event) {thisinst.mouseMoved(event)},false);
-	document.body.addEventListener('mouseover',function (event) {thisinst.mouseMoved(event)},false);
-	document.body.addEventListener('mouseout',function (event) {thisinst.mouseMoved(event)},false);
-	document.body.addEventListener('mousedown',function (event) {thisinst.mousePressed(event,"body")},false);
-	document.body.appendChild(el);
+	this._addElement(document.getElementsByTagName('head')[0], 'link', 'JsPanel_Link',null,{type:'text/css',href:'JsPanel.css',rel:'stylesheet'});
+	this.setTheme(this.themedir);
+	this.wipe();
+	if (x) this.state.x = parseInt(x) + 'px';
+	if (y) this.state.x = parseInt(y) + 'px';
+	if (ht) this.state.x = parseInt(ht) + 'px';
+	if (wd) this.state.x = parseInt(wd) + 'px';
+	this.element.style.left = this.state.x;
+	this.element.style.top = this.state.y;
+	this.element.style.height = this.state.ht;
+	this.element.style.width = this.state.wd;
+	this._addEventListener(document.getElementsByTagName('body')[0],'mousemove',function(event) {thisinst.mouseMoved(event)},false);
+	this._addEventListener(document.getElementsByTagName('body')[0],'mouseover',function(event) {thisinst.mouseMoved(event)},false);
+	this._addEventListener(document.getElementsByTagName('body')[0],'mouseout',function(event) {thisinst.mouseMoved(event)},false);
+	this._addEventListener(document.getElementsByTagName('body')[0],'mousedown',function(event) {thisinst.mousePressed(event,"body")},false);
+	document.getElementsByTagName('body')[0].appendChild(el);
+}
+
+JsPanel.prototype.setTitle = function (str)
+{
+	this.title.text = (str)?str:" ";
+	if (this.title.element) this.title.element.innerHTML = this.title.text;
 }
 
 JsPanel.prototype.show = function ()
 {
 	if (!this.element || !this.element.constructor.toString().match(/HTML([a-zA-Z0-9]*)Element/)) throw "showPanel(): Build Panel first";
+	this.element.className = 'JsPanel_container';
+ 	this.element.style.left = this.state.x;
+	this.element.style.top = this.state.y;
+	this.element.style.height = this.state.ht;
+	this.element.style.width = this.state.wd; 
 }
 
-JsPanel.prototype.makeEvent = function (obj,funcname)
+JsPanel.prototype.hide = function ()
 {
-	return obj[funcname](e,this);
+ 	if (!this.element || !this.element.constructor.toString().match(/HTML([a-zA-Z0-9]*)Element/)) throw "showPanel(): Build Panel first";
+	this.element.className = 'JsPanel_hidden'; 
 }
-
